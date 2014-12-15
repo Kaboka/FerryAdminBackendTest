980ac4be51081575dd4fc3f78f08f5a9e4770531
Kasper <Kasper@10.50.137.78>
2014-12-15 09:25:12 +0100

    first commit

---
+++ test/BackendTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import dk.cphbusiness.ferry.ManageFerriesTest;
+import dk.cphbusiness.test.FerryManagerHolder;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kasper
+ */
+public class BackendTest extends ManageFerriesTest {
+    
+
+    @Override
+    @Before
+    public void setUp() {
+        FerryManagerHolder.manager = new FerryAdminManager();
+        super.setUp();
+    }
+    
+}
